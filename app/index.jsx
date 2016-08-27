386cbc175b78b338435763fed527c3c1317e815b
stellar91 <muyuxingguang@163.com>
2016-08-27 20:36:01 +0800

    添加图片控制按钮

--- app/index.jsx
+++ app/index.jsx
@@ -86,6 +86,42 @@ class ImgFigure extends React.Component{
 };
 
 
+//控制组件
+class ControllerUnit extends  React.Component{
+    constructor(props){
+        super(props);
+        this.handleClick=this.handleClick.bind(this);
+    }
+    handleClick(e){
+        //如果点击的是当前正在选中态的按钮，则翻转图，否则对应图片居中
+        if(this.props.arrange.isCenter){
+            this.props.inverse();
+        }else{
+            this.props.center();
+        }
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    render(){
+        var controllerUnitClassName="controller-unit";
+        //如果对应的是居中的图片，显示控制按钮的居中态
+        if (this.props.arrange.isCenter){
+            controllerUnitClassName+=" is-center";
+            //如果同时对应的是翻转图片，显示控制按钮的翻转态
+            if(this.props.arrange.isInverse){
+                controllerUnitClassName+=" is-inverse";
+            }
+        }
+        return (
+            <span className={controllerUnitClassName} onClick={this.handleClick}></span>
+        )
+
+    }
+
+};
+
+
 class  GalleryByReactApp extends React.Component{
     constructor(pros){
         super(pros);
@@ -285,7 +321,8 @@ class  GalleryByReactApp extends React.Component{
 
                 }
             }
-           imgFigures.push(<ImgFigure data={value} ref={'imgFigure'+index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>);
+           imgFigures.push(<ImgFigure key={index} data={value} ref={'imgFigure'+index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>);
+            controllerUnits.push(<ControllerUnit key={index} arrange={this.state.imgsArrangeArr[index]} inverse={this.inverse(index)} center={this.center(index)}/>)
         }.bind(this));
         return (
             <section className="stage" ref="stage">
